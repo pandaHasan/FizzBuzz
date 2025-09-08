c19093c5884b7c41a66e07232dab5075054a4506
pandaHasan <hasanhdgamerr@gmail.com>
2025-09-08 10:12:03 -0400

    multiples extracted

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -3,8 +3,12 @@ package lab1;
 public class Multiples {
     public static void main(String[] args) {
 
-        for(int i=1;i<=100;i++){
-            if(i % 3 == 0 | i%5 == 0){
+
+    }
+
+    public static void multiples(int n, int a, int b) {
+        for(int i=1;i<=n;i++){
+            if(i % a == 0 | i%b == 0){
                 System.out.println(i);
             }
         }
